55f6f91929482c11d075e10e8d73702b2141f777
Roxxouille <ralflegranddu56@gmail.com>
2020-06-17 17:01:07 +0200

    Gestions des messages d'erreur et factorisati on de la commande roll

--- commands/Dice/roll.js
+++ commands/Dice/roll.js
@@ -1,5 +1,8 @@
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const rollTheDice = (maxDiceValue) => Math.floor( Math.random() * maxDiceValue ) + 1;
+
 module.exports.run = (client, message, args) => {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const error = () => message.channel.send('Mauvaise utilisation de la commande')
   let dice = args;
   let firstNumber = /[^d]*/; //capture données avant 'd'
   let lastNumber = /(?<=d).*/; //capture données après 'd'
@@ -8,65 +11,83 @@ module.exports.run = (client, message, args) => {
   let lastPart = lastNumber.exec(dice);
   let firstPartInt = parseInt(firstPart, 10);
   let lastPartInt = parseInt(lastPart, 10);
-  if(dTest.test(dice)){
-    if(firstPartInt === 1 && lastPartInt === 100){
-    let diceRoll = Math.floor( Math.random() * lastPartInt ) + 1;
 
-    if(diceRoll <= 5) {
-      message.reply(`WOAWWWW Tu as fait une réussite critique ! ${diceRoll}`);
+
+  // Si il y a un 'd' dans la commande : 
+  if(dTest.test(dice)){
+
+    if (isNaN(firstPartInt)  || isNaN(lastPartInt) || firstPartInt <= 0 || lastPartInt <= 0){
+      error();
       return;
     }
-    if(diceRoll >= 96) {
-      message.reply(`OOF tu as fait un échec critique ! ${diceRoll}`);
-      return;
-    }
-    else {
-      message.reply(`Alors ça passe ? ${diceRoll}`);
-    }
+
+    if(firstPartInt === 1 && lastPartInt === 100){
+      let diceRoll = rollTheDice(lastPartInt);
+
+      if(diceRoll <= 5) {
+        message.reply(`WOAWWWW Tu as fait une réussite critique ! ${diceRoll}`);
+        return;
+      }
+      if(diceRoll >= 96) {
+        message.reply(`OOF tu as fait un échec critique ! ${diceRoll}`);
+        return;
+      }
+      else {
+        message.reply(`Alors ça passe ? ${diceRoll}`);
+      }
     }
     else {
       let roll = [];
       for(let i = 1; i <= firstPartInt; i++ ){
-        let diceRoll = Math.floor( Math.random() * lastPartInt ) + 1;
+        let diceRoll = rollTheDice(lastPartInt);
         roll.push(diceRoll);
       }
       message.reply(`Résultats : ${roll.join(' + ')} = ${roll.reduce(reducer)}`);
     }
   }
+  // Si il n'y a pas de 'd' dans la commande :
   else {
-    let diceRoll = Math.floor( Math.random() * dice ) + 1;
-    if (dice == 100){
-        
 
+    if(isNaN(dice) || dice <= 0){
+      error();
+      return;
+    }
+    
+    let diceRoll = rollTheDice(dice);
+
+    if (dice == 100){
+      
       if(diceRoll <= 5) {
         message.reply(`WOAWWWW Tu as fait une réussite critique ! ${diceRoll}`);
         return;
       }
+
       if(diceRoll >= 96) {
         message.reply(`OOF tu as fait un échec critique ! ${diceRoll}`);
         return;
       }
+
       else {
         message.reply(`Alors ça passe ? ${diceRoll}`);
+        return;
       }
     }
     if (dice == 20) {
-      if(diceRoll <= 5) {
+      if(diceRoll <= 3) {
         message.reply(`WOAWWWW Tu as fait une réussite critique ! ${diceRoll}`);
         return;
       }
-      if(diceRoll >= 96) {
+      if(diceRoll >= 18) {
         message.reply(`OOF tu as fait un échec critique ! ${diceRoll}`);
         return;
       }
-      else {
+      else 
+      {
         message.reply(`Alors ça passe ? ${diceRoll}`);
+        return;
       }
     }
-    else {
-      message.reply(`Alors ça passe ? ${diceRoll}`);
-    }
-    }
+  }
 };
 
 module.exports.help = {
